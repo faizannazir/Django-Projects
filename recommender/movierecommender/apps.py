e7b5bad997d2ef0ac4a6d3e882c4d0435b541243
Faizan Nazir <faizannazir289@gmail.com>
2023-08-10 22:42:53 +0500

    Delete recommender directory

--- recommender/movierecommender/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class MovierecommenderConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'movierecommender'
--- recommender/movierecommender/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-
-urlpatterns = [
-    # route is a string contains a URL pattern
-path(route='', view=views.movie_recommendation_view, name='recommendations'),
-
-]
